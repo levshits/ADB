4baed42c59415824e7886b9ecb7d5c8f8289af1c
levshits <levshits.l@gmail.com>
2015-12-01 14:02:14 +0300

    Fixed levels gradation

--- ADB.Common/Immutable/PriorityLevels.cs
+++ ADB.Common/Immutable/PriorityLevels.cs
@@ -2,6 +2,9 @@
 {
     public class PriorityLevels
     {
+        public const int ZERO_LEVEL = 0;
         public const int FIRST_LEVEL = 1;
+        public const int SECOND_LEVEL = 2;
+        public const int THIRD_LEVEL = 3;
     }
 }
