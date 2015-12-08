b75bb2739c787c75c2890c7ad4171c0512a84463
levshits <levshits.l@gmail.com>
2015-12-08 07:34:44 +0300

    Added standart accounts

--- ADB.Logic/Blo/TransactionHistoryBlo.cs
+++ ADB.Logic/Blo/TransactionHistoryBlo.cs
@@ -11,8 +11,12 @@ namespace ADB.Logic.Blo
 {
     public class TransactionHistoryBlo: BloBase<TransactionHistoryEntity>
     {
-        public const int BANK_ACCOUNT = 26;
-        public const int REPOSITORY_ACCOUNT = 27;
+        public const int BANK_ACCOUNT_BYR = 26;
+        public const int REPOSITORY_ACCOUNT_BYR = 27;
+        public const int BANK_ACCOUNT_USD = 47;
+        public const int REPOSITORY_ACCOUNT_USD = 49;
+        public const int BANK_ACCOUNT_EUR = 48;
+        public const int REPOSITORY_ACCOUNT_EUR = 51;
 
         public AdbRepository AdbRepository => (AdbRepository) Repository;
 
@@ -40,7 +44,7 @@ namespace ADB.Logic.Blo
                     CreateTime = DateTime.Now,
                     CurrencyType = (int) dto.CurrencyType,
                     FromAccount = null,
-                    ToAccount = REPOSITORY_ACCOUNT
+                    ToAccount = GetRepositoryAccount(dto.CurrencyType)
                 });
             AdbRepository.TransactionHistoryData.Save(
                 new TransactionHistoryEntity()
@@ -48,7 +52,7 @@ namespace ADB.Logic.Blo
                     Count = dto.Summ,
                     CreateTime = DateTime.Now,
                     CurrencyType = (int) dto.CurrencyType,
-                    FromAccount = REPOSITORY_ACCOUNT,
+                    FromAccount = GetRepositoryAccount(dto.CurrencyType),
                     ToAccount = dto.MainAccountId
                 });
             AdbRepository.TransactionHistoryData.Save(
@@ -58,9 +62,40 @@ namespace ADB.Logic.Blo
                     CreateTime = DateTime.Now,
                     CurrencyType = (int) dto.CurrencyType,
                     FromAccount = dto.MainAccountId,
-                    ToAccount = BANK_ACCOUNT
+                    ToAccount = GetBankAccount(dto.CurrencyType)
                 });
+            AccountEntity account = AdbRepository.AccountData.GetEntityById(GetBankAccount(dto.CurrencyType));
+            account.Balance += dto.Summ;
+            AdbRepository.AccountData.Save(account);
             return context.PreviousResult;
+        }
+
+        private static int GetRepositoryAccount(CurrencyTypeEnum currencyType)
+        {
+            switch (currencyType)
+            {
+                case CurrencyTypeEnum.BYR:
+                    return REPOSITORY_ACCOUNT_BYR;
+                case CurrencyTypeEnum.EUR:
+                    return REPOSITORY_ACCOUNT_EUR;
+                case CurrencyTypeEnum.USD:
+                    return REPOSITORY_ACCOUNT_USD;
+            }
+            return REPOSITORY_ACCOUNT_BYR;
+        }
+
+        private static int GetBankAccount(CurrencyTypeEnum currencyType)
+        {
+            switch (currencyType)
+            {
+                case CurrencyTypeEnum.BYR:
+                    return BANK_ACCOUNT_BYR;
+                case CurrencyTypeEnum.EUR:
+                    return BANK_ACCOUNT_EUR;
+                case CurrencyTypeEnum.USD:
+                    return BANK_ACCOUNT_USD;
+            }
+            return REPOSITORY_ACCOUNT_BYR;
         }
 
         private ExecutionResult InitCreditAccount(SaveCreditContractRequest request, ExecutionContext context)
@@ -70,13 +105,18 @@ namespace ADB.Logic.Blo
             {
                 return null;
             }
+
+            AccountEntity account = AdbRepository.AccountData.GetEntityById(GetBankAccount(dto.CurrencyType));
+            account.Balance -= dto.Summ;
+            AdbRepository.AccountData.Save(account);
+
             AdbRepository.TransactionHistoryData.Save(
                 new TransactionHistoryEntity()
                 {
                     Count = dto.Summ,
                     CreateTime = DateTime.Now,
                     CurrencyType = (int)dto.CurrencyType,
-                    FromAccount = BANK_ACCOUNT,
+                    FromAccount = GetBankAccount(dto.CurrencyType),
                     ToAccount = dto.MainAccountId
                 });
             AdbRepository.TransactionHistoryData.Save(
@@ -86,7 +126,7 @@ namespace ADB.Logic.Blo
                     CreateTime = DateTime.Now,
                     CurrencyType = (int)dto.CurrencyType,
                     FromAccount = dto.MainAccountId,
-                    ToAccount = REPOSITORY_ACCOUNT
+                    ToAccount = GetRepositoryAccount(dto.CurrencyType)
                 });
             AdbRepository.TransactionHistoryData.Save(
                 new TransactionHistoryEntity()
@@ -94,7 +134,7 @@ namespace ADB.Logic.Blo
                     Count = dto.Summ,
                     CreateTime = DateTime.Now,
                     CurrencyType = (int)dto.CurrencyType,
-                    FromAccount = REPOSITORY_ACCOUNT,
+                    FromAccount = GetRepositoryAccount(dto.CurrencyType),
                     ToAccount = null
                 });
             return context.PreviousResult;
